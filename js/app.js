eee8643084b3b65ccc16189d45a25371677deb0c
woosie10 <alan.woosnam10@gmail.com>
2016-02-23 16:26:00 +0000

    design first draft
    containers created and colours selected

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
 var printrApp = angular.module( 'printrApp', [] );
 
  
+
+
+//set height to full page
+printrApp.directive('resize', function ($window) {
+    return function (scope) {
+
+      scope.style = function () {
+          return { 
+              'min-height': ($window.innerHeight) + 'px',
+
+          };
+      };
+
+    
+    }
+});
--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,12 @@
 
 printrApp.controller('dashboardCtrl', function($scope) {
 
+	$scope.totals = [];
 
+	$scope.totals.users = 251;
+	$scope.totals.slicedModels = 1156;
+	$scope.totals.printHours = 6532;
 
+	
 });
 
