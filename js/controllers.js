f16a28ba4ebba6485e9ec90c7806cadacc8aaa7d
woosie10 <alan.woosnam10@gmail.com>
2016-02-24 20:54:05 +0000

    live charts created
    live charts created and running off random data

--- js/controllers.js
+++ js/controllers.js
@@ -1,5 +1,5 @@
 
-printrApp.controller('dashboardCtrl', function($scope, weeklyTotals, percentageDiff) {
+printrApp.controller('totalsCtrl', function($scope, weeklyTotals, percentageDiff) {
 
 	
 	$scope.weeklyTotals = [];
@@ -7,9 +7,9 @@ printrApp.controller('dashboardCtrl', function($scope, weeklyTotals, percentageD
 	$scope.usersTotals = [];
 	$scope.modelsSlicedTotals = [];
 	$scope.printHoursTotals = [];
-	$scope.currentTotals = [];
+	$scope.totals = [];
 	$scope.previousTotals = [];
-	
+
 
 	weeklyTotals.getData().then(function(data){
 
@@ -22,12 +22,12 @@ printrApp.controller('dashboardCtrl', function($scope, weeklyTotals, percentageD
 			$scope.printHoursTotals.push(item.printHours);
        	})
 
-       	$scope.currentTotals = $scope.weeklyTotals[$scope.weeklyTotals.length - 1];
+       	$scope.totals = $scope.weeklyTotals[$scope.weeklyTotals.length - 1];
        	$scope.previousTotals = $scope.weeklyTotals[$scope.weeklyTotals.length - 2];
 
-       	$scope.currentTotals.usersDiff = percentageDiff.calc($scope.currentTotals.users, $scope.previousTotals.users);
-       	$scope.currentTotals.modelsSlicedDiff = percentageDiff.calc($scope.currentTotals.modelsSliced, $scope.previousTotals.modelsSliced);
-       	$scope.currentTotals.printHoursDiff = percentageDiff.calc($scope.currentTotals.printHours, $scope.previousTotals.printHours);
+       	$scope.totals.usersDiff = percentageDiff.calc($scope.totals.users, $scope.previousTotals.users);
+       	$scope.totals.modelsSlicedDiff = percentageDiff.calc($scope.totals.modelsSliced, $scope.previousTotals.modelsSliced);
+       	$scope.totals.printHoursDiff = percentageDiff.calc($scope.totals.printHours, $scope.previousTotals.printHours);
 
 
 
@@ -39,6 +39,13 @@ printrApp.controller('dashboardCtrl', function($scope, weeklyTotals, percentageD
 	    chart: {
 	      	type: 'spline'
 	    },
+	    plotOptions: {
+		    series: {
+		        marker: {
+		            enabled: false
+		        }
+		    }
+		},
 	    xAxis: {
             categories: $scope.xAxis
         },
@@ -57,7 +64,7 @@ printrApp.controller('dashboardCtrl', function($scope, weeklyTotals, percentageD
 		  
 		series: [
 		    {
-		      	name: 'No. of Users',
+		      	name: 'Users Total',
 		      	data: $scope.usersTotals,
 				id: 'series-0',
 				connectNulls: true,
@@ -77,7 +84,7 @@ printrApp.controller('dashboardCtrl', function($scope, weeklyTotals, percentageD
 		  
 		series: [
 		    {
-		      	name: 'No. Models Sliced',
+		      	name: 'Models Sliced Total',
 		      	data: $scope.modelsSlicedTotals,
 				connectNulls: true,
 				id: 'series-1',
@@ -96,7 +103,7 @@ printrApp.controller('dashboardCtrl', function($scope, weeklyTotals, percentageD
 		  
 		series: [
 		    {
-		      	name: 'No. Print Hours',
+		      	name: 'Print Hours Total',
 		      	data: $scope.printHoursTotals,
 				type: 'spline',
 				id: 'series-2',
@@ -113,3 +120,163 @@ printrApp.controller('dashboardCtrl', function($scope, weeklyTotals, percentageD
 
 });
 
+
+
+
+
+
+
+printrApp.controller('livedataCtrl', function($scope, $filter) {
+
+
+	$scope.devices = [50];
+	$scope.users = [75];
+	$scope.jobs = [25];
+	$scope.xAxis = [$filter('date')(new Date(), 'HH:mm:ss')];
+	$scope.currentTotals = [];
+
+	$scope.lineChartOptions = {
+	    chart: {
+	      	type: 'spline'
+	    },
+	    plotOptions: {
+		    series: {
+		        animation: false,
+		        marker: {
+		            enabled: false
+		        }
+		    }
+		},
+	    xAxis: {
+            categories: $scope.xAxis
+        },
+        title: {
+			text: ''
+		},
+		credits: {
+			enabled: false
+		}
+	};
+
+
+	$scope.devicesConnectedConfig = {
+
+		options: $scope.lineChartOptions,
+		  
+		series: [
+		    {
+		      	name: 'Devices Connected',
+		      	data: $scope.devices,
+				id: 'series-0',
+				connectNulls: true,
+				type: 'spline',
+				lineWidth: 2,
+				color: '#1EB1E6'
+		    }
+		]	
+
+    };
+
+
+    $scope.usersConnectedConfig = {
+
+		options: $scope.lineChartOptions,
+		  
+		series: [
+		    {
+		      	name: 'Users Connected',
+		      	data: $scope.users,
+				connectNulls: true,
+				id: 'series-1',
+				type: 'spline',
+				lineWidth: 2,
+				color: '#888998'
+		    }
+		]		
+
+    };
+
+
+    $scope.currentJobsConfig = {
+
+		options: $scope.lineChartOptions,
+		  
+		series: [
+		    {
+		      	name: 'Current Jobs',
+		      	data: $scope.jobs,
+				connectNulls: true,
+				id: 'series-1',
+				type: 'spline',
+				lineWidth: 2,
+				color: '#3E4259'
+		    }
+		]		
+
+    };
+
+
+
+    $scope.currentTotals.devices = $scope.devices[0];
+    $scope.currentTotals.users = $scope.users[0];
+    $scope.currentTotals.jobs = $scope.jobs[0];
+
+
+    function updateChart(chart, total){
+
+    	var newTime = $filter('date')(new Date(), 'HH:mm:ss');
+
+        $scope.dataSeries = chart.series[0].data;
+        $scope.dataSeries.push(total);
+        
+        if($scope.dataSeries.length>30){
+        	$scope.dataSeries.splice(0,1);
+    	}
+
+        
+        $scope.dataAxis = chart.options.xAxis.categories;
+        $scope.dataAxis.push(newTime);
+
+        if($scope.dataAxis.length>30){
+        	$scope.dataAxis.splice(0,1);
+    	}
+
+	};
+
+
+
+    setInterval(function () {
+
+    	$scope.currentTotals.devices += Math.round(Math.random() * 2 - 1);
+
+        if($scope.currentTotals.devices<0){
+        	$scope.currentTotals.devices = 1;
+        }
+
+    	updateChart($scope.devicesConnectedConfig, $scope.currentTotals.devices);
+
+
+    	$scope.currentTotals.users += Math.round(Math.random() * 2 - 1);
+
+        if($scope.currentTotals.users<0){
+        	$scope.currentTotals.users = 1;
+        }
+
+    	updateChart($scope.usersConnectedConfig, $scope.currentTotals.users);
+
+
+    	$scope.currentTotals.jobs += Math.round(Math.random() * 2 - 1);
+
+        if($scope.currentTotals.jobs<0){
+        	$scope.currentTotals.jobs = 1;
+        }
+
+    	updateChart($scope.currentJobsConfig, $scope.currentTotals.jobs);
+
+        $scope.$apply();
+
+    }, 2000);
+
+
+
+});
